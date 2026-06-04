a207022af598858c68f511e956beea02317ff1b2
Mr. Arceni <nikitin.arceny@gmail.com>
2026-06-04 20:21:47 +0200

    auto-formatting

--- src/Commands/Latency.ts
+++ src/Commands/Latency.ts
@@ -1,13 +1,15 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 import type { ICommand } from "../Interfaces/ICommand.js";
 
 export const command: ICommand = {
-    data: new SlashCommandBuilder()
-        .setName('latency')
-        .setDescription('Shows latency in execution commands, including ping'),
+  data: new SlashCommandBuilder()
+    .setName("latency")
+    .setDescription("Shows latency in execution commands, including ping"),
 
-    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-        let nowSys = performance.now();
-        await interaction.reply(`Time elapsed: ${Date.now().valueOf() - interaction.createdTimestamp} ms; Includes: ${(performance.now() - nowSys).toPrecision(3)} ms on executing command`);
-    }
-}
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    let nowSys = performance.now();
+    await interaction.reply(
+      `Time elapsed: ${Date.now().valueOf() - interaction.createdTimestamp} ms; Includes: ${(performance.now() - nowSys).toPrecision(3)} ms on executing command`,
+    );
+  },
+};
--- src/Types/Discordjs/ClientExtension.d.ts
+++ src/Types/Discordjs/ClientExtension.d.ts
@@ -2,7 +2,7 @@ import { Collection } from "discord.js";
 import { ICommand } from "../../Interfaces/ICommand.js";
 
 declare module "discord.js" {
-    export interface Client {
-        commands: Collection<string, ICommand>;
-    }
+  export interface Client {
+    commands: Collection<string, ICommand>;
+  }
 }
